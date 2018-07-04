bd17193fccaed2e9f594e486b17a3396454fc22c
Shashi Deshetti <deshetti@gmail.com>
2018-07-04 12:34:13 -0500

    Added Articlet entity

---
+++ src/main/java/com/factly/degaresearch/domain/Article.java
@@ -0,0 +1,96 @@
+package com.factly.degaresearch.domain;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A Article.
+ */
+@Entity
+@Table(name = "article")
+public class Article implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @NotNull
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @NotNull
+    @Column(name = "message", nullable = false)
+    private String message;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Article title(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Article message(String message) {
+        this.message = message;
+        return this;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Article article = (Article) o;
+        if (article.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), article.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+            "id=" + getId() +
+            ", title='" + getTitle() + "'" +
+            ", message='" + getMessage() + "'" +
+            "}";
+    }
+}
---
+++ src/main/java/com/factly/degaresearch/service/mapper/ArticleMapper.java
@@ -0,0 +1,24 @@
+package com.factly.degaresearch.service.mapper;
+
+import com.factly.degaresearch.domain.*;
+import com.factly.degaresearch.service.dto.ArticleDTO;
+
+import org.mapstruct.*;
+
+/**
+ * Mapper for the entity Article and its DTO ArticleDTO.
+ */
+@Mapper(componentModel = "spring", uses = {})
+public interface ArticleMapper extends EntityMapper<ArticleDTO, Article> {
+
+
+
+    default Article fromId(Long id) {
+        if (id == null) {
+            return null;
+        }
+        Article article = new Article();
+        article.setId(id);
+        return article;
+    }
+}
